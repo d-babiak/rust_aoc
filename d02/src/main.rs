39e6bf64f9f8f6b91269b9575f92dfae7a8dc720
Daniel Babiak <d.babiak@gmail.com>
2019-04-20 10:44:48 -0700

    Day 2 Part 2

--- d02/src/main.rs
+++ d02/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -33,7 +33,34 @@ fn d2_p1(xs: &Vec<String>) -> usize {
     twos * threes
 }
 
+fn ndiff(s1: &str, s2: &str) -> usize {
+    s1.chars()
+        .zip(s2.chars())
+        .filter(|(c1, c2)| c1 != c2)
+        .count()
+}
+
+fn intersect(s1: &str, s2: &str) -> String {
+    s1.chars()
+        .zip(s2.chars())
+        .filter(|(c1, c2)| c1 == c2)
+        .map(|(c1, _c2)| c1 )
+        .collect()
+}
+
+fn d2_p2(xs: &Vec<String>) -> String {
+    for s1 in xs.iter() {
+        for s2 in xs.iter() {
+            if ndiff(s1, s2) == 1 {
+                return intersect(s1, s2);
+            }
+        }
+    }
+    panic!("AHHHH")
+}
+
 fn main() {
     let lines = parse_input("/Users/dmb/rust_aoc/inputs/d2.txt");
-    println!("{}", d2_p1(&lines))
+    println!("{}", d2_p1(&lines));
+    println!("{}", d2_p2(&lines))
 }
