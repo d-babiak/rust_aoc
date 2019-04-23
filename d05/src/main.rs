424f07dbc88e9953e18bc75672f8da42a6a36565
Daniel Babiak <d.babiak@gmail.com>
2019-04-23 12:09:06 -0700

    Day 5 Part 2

--- d05/src/main.rs
+++ d05/src/main.rs
@@ -22,6 +22,15 @@ fn d5_p1(s: &str) -> usize {
     stack.len()
 }
 
+fn alphabet() -> Vec<char> {
+    //  https://stackoverflow.com/questions/45343345/is-there-a-simple-way-to-generate-the-lowercase-and-uppercase-english-alphabet-i/45344161
+    let letters = (b'a'..=b'z')             // Start as u8
+        .map(|c| c as char)                     // Convert all to chars
+        .filter(|c| c.is_alphabetic()) // Filter only alphabetic chars
+        .collect::<Vec<_>>();
+    letters
+}
+
 fn without_char(s: &str, c: char) -> String {
     let c = c.to_ascii_lowercase();
 
@@ -30,12 +39,25 @@ fn without_char(s: &str, c: char) -> String {
         .collect()
 }
 
+fn d5_p2(s: &str) -> usize {
+    // is there a "max int" type thing?
+    let mut maybe_n: Option<usize> = None;
+    for c in alphabet().iter() {
+        let len_without_c = d5_p1(&without_char(s, c.clone()));
+        println!("Len without {}: {}", c, len_without_c);
+        maybe_n = match maybe_n {
+            None => Some(len_without_c),
+            Some(n) => Some(n.min(len_without_c)),
+        }
+    }
+    maybe_n.unwrap()
+}
+
 fn main() {
     let s = std::fs::read_to_string("../inputs/d5.txt").unwrap();
     let s = s.trim();
-    let n = d5_p1(s);
-    println!("{}", n);
-    let s2 = "dabAcCaCBAcCcaDA";
-    let s3 = without_char(s2, 'a');
-    println!("{}", s3);
+    let n1 = d5_p1(s);
+    println!("{}", n1);
+    let n2 = d5_p2(s);
+    println!("{}", n2);
 }
